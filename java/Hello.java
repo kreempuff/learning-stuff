fb566022b0a330ae389bdfb3739e4d315c7ef4a9
lentusignavus <ignavus.ii@gmail.com>
2015-12-21 23:16:53 -0600

    The main procedure is static From my understanding it exists separately from the rest of the java program. The class it is within needs to be instantiated before any values can be used

--- java/Hello.java
+++ java/Hello.java
@@ -1,9 +1,8 @@
-import java.util.*;
 
 public class Hello {
-
   public static void main(String[] args) {
+    World earth = new World();
     System.out.println("Hello cup of joe");
+    System.out.println(earth.toString());
   }
-  
 }
---
+++ java/World.java
@@ -0,0 +1,9 @@
+public class World {
+  private int year = 3;
+
+
+  public String toString(){
+    String world = "World";
+    return world;
+  }
+}
